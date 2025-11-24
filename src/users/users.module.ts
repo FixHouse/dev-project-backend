af7031ae8215f992a8477d962271dac495efc0f4
Vitaliy <vitalikxaoc@gmail.com>
2025-11-24 13:34:55 +0100

    Prisma fixed, schema configured, client generated

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { PrismaModule } from '../prisma/prisma.module';  // ⬅ додаємо
 
 @Module({
+  imports: [PrismaModule],                           // ⬅ додаємо
   providers: [UsersService],
-  controllers: [UsersController]
+  controllers: [UsersController],
 })
 export class UsersModule {}
